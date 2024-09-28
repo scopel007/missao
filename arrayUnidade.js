5f756980178068591ec42ab0a3213a64e99a22c3
scopel007 <128197237+scopel007@users.noreply.github.com>
2024-09-27 21:44:29 -0300

    .

---
+++ arrayUnidade.js
@@ -0,0 +1,7 @@
+var unidades= [“Segurança da Informação”, “Game Designer”, “Ciência s Computação”];
+
+function unidadeEscolhida(numero){
+return unidades[numero];
+}
+
+console.log(unidadeEscolhida(0));
