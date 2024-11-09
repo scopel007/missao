d0c8900e4bdf144c8e3ee2882eff5a51bd58c229
scopel007 <128197237+scopel007@users.noreply.github.com>
2024-11-08 22:15:58 -0300

    .

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-const boataoJogarNovamente = document.querySelector(".novamente-btn")
+const boataoJogarNovamente = document.querySelector(".novamente-btn");
 
 let atual = 0;
 let perguntaAtual;
@@ -43,11 +43,10 @@ function mostraResultado() {
     caixaPerguntas.textContent = "Em 2049...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
-}
-
 
 }
 
-
-
+function jogaNovamente(){
+    atual = 0;
+}
 mostraPergunta();
