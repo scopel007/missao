67a3a6e9f9c6a2810bbe8cb4cce696bee14f3129
scopel007 <128197237+scopel007@users.noreply.github.com>
2024-11-08 22:12:15 -0300

    .

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
- 
+const boataoJogarNovamente = document.querySelector(".novamente-btn")
+
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
